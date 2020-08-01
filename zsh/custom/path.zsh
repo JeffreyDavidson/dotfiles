29da851530600ed6d6cc62ff7e3be3b35a3f3a4a
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-08-01 15:45:19 -0400

    Add actual path file for zsh

---
+++ zsh/custom/path.zsh
@@ -0,0 +1,26 @@
+# Load Composer tools
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+
+# Load Node global installed binaries
+export PATH="$HOME/.node/bin:$PATH"
+
+# Use project specific binaries before global ones
+export PATH="node_modules/.bin:vendor/bin:$PATH"
+
+# Add dotfiles shell scripts to path
+export PATH="$HOME/.dotfiles/bin:$PATH"
+
+# Add visual studio code to PATH
+if [[ -d "/Applications/Visual Studio Code.app" ]];
+    then export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH";
+fi
+
+# Add visual studio code insiders to PATH
+if [[ -d "/Applications/Visual Studio Code - Insiders.app" ]];
+    then export PATH="/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin:$PATH";
+fi
+
+# Add tower to PATH
+if [[ -d "/Applications/Tower.app" ]];
+    then export PATH="/Applications/Tower.app/Contents/Resources/app/bin:$PATH";
+fi
