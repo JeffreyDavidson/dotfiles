bc19d43d01c92de0b578b5d3e5549ee149a2c135
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-01-25 10:02:55 -0500

    Symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -18,3 +18,13 @@ else
   echo "Enter user password to change login shell"
   chsh -s '/opt/homebrew/bin/zsh'
 fi
+
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+
+  # I'd like for this to work instead.
+  # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh
+fi
