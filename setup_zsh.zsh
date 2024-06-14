93e07eb52637c6fd512fd7c196679b4e36e06b74
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-06-14 19:26:12 -0400

    Symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -8,13 +8,24 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
   echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
 else
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
   echo '$SHELL is already /opt/homebrew/bin/zsh'
 else
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  # Looked cute, might delete later, idk
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+
+  # I'd like for this to work instead.
+  # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh
 fi
