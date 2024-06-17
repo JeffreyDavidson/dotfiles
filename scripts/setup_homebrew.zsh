89268b37038be17c0837f767848e37bdb3cf4d87
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-06-17 12:38:37 -0400

    Relocate only used function to setup_hoembrew.zsh file

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
+function exists() {
+  # `command -v` is similar to `which`
+  # https://stackoverflow.com/a/677212/1341838
+  command -v $1 >/dev/null 2>&1
+
+  # More explicitly written:
+  # command -v $1 1>/dev/null 2>/dev/null
+}
+
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
 if exists brew; then
