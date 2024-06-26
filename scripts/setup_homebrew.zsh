8ce2a33fefe8bac20a3b7f642b3b20c7d72aefbe
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-06-25 21:48:57 -0400

    Install Better Keybindings extension through vsix file

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -34,5 +34,6 @@ brew bundle --verbose --file=./homebrew/Masfile
 
 echo "Installing VS Code Extensions"
 cat vscode/extensions-list | xargs -L 1 code --install-extension
+code --install-extension vscode/extensions/Better\ Keybindings/Better-Keybindings-0.1.2.vsix
 
 echo "\n<<< Finished Homebrew Setup >>>\n"
