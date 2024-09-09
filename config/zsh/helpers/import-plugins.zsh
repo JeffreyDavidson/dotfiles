4566b2e198f21cc842e25bab056f3811ff1c680f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-09 09:53:45 -0400

    Add more zsh plugins and alphabetize

--- config/zsh/helpers/import-plugins.zsh
+++ config/zsh/helpers/import-plugins.zsh
@@ -1,25 +1,34 @@
 #!/usr/bin/env zsh
 
-# Syntax highlighting for commands
-antigen bundle zsh-users/zsh-syntax-highlighting
-
 # Quickly jump into frequently used directories
 antigen bundle agkozak/zsh-z
 
 # Replace zsh default completion menu with fzf
 antigen bundle Aloxaf/fzf-tab
 
+# Run mkdir and cd in one command
+antigen bundle caarlos0/zsh-mkc
+
 # Remind you to use existing aliases
 antigen bundle MichaelAquilina/zsh-you-should-use
 
-# Search history
-antigen bundle zsh-users/zsh-history-substring-search
+# Run artisan from anywhere in a directory
+antigen bundle jessarcher/zsh-artisan
+
+# Open current repository in browser
+antigen bundle paulirish/git-open
+
+# Auto suggestions from history
+antigen bundle zsh-users/zsh-autosuggestions
 
 # Extra zsh completions
 antigen bundle zsh-users/zsh-completions
 
-# Auto suggestions from history
-antigen bundle zsh-users/zsh-autosuggestions
+# Search history
+antigen bundle zsh-users/zsh-history-substring-search
+
+# Syntax highlighting for commands
+antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Tell antigen that you're done
 antigen apply
