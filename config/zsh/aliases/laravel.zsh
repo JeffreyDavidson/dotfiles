81624d0b8e14ebe0ce59e8c9c341583ca7f108cd
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-09 09:54:16 -0400

    Add alias for zsh-artisan plugin

--- config/zsh/aliases/laravel.zsh
+++ config/zsh/aliases/laravel.zsh
@@ -5,6 +5,7 @@ alias seed="php artisan seed"
 alias srv="php artisan serve"
 alias mfs="php artisan migrate:fresh --seed"
 alias sail="./vendor/bin/sail"
+alias a="artisan"
 
 # Larastan
 alias larastan="vendor/bin/phpstan analyse"
