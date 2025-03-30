3d314b7bda9900e1aae893556b62031bcc8956bf
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-03-30 17:39:28 -0400

    Add missing exists function

--- scripts/setup_node.zsh
+++ scripts/setup_node.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
+function exists() {
+  # `command -v` is similar to `which`
+  # https://stackoverflow.com/a/677212/1341838
+  command -v $1 >/dev/null 2>&1
+
+  # More explicitly written:
+  # command -v $1 1>/dev/null 2>/dev/null
+}
+
 echo "\n<<< Starting Node Setup >>>\n"
 
 # Node versions are managed with `n`, which is in the Brewfile.
