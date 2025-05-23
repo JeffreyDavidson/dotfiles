df5720272f757c8624bc4d03f2ec9809850edc0b
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-05-23 14:23:18 -0400

    Remove herd from artisan aliases

--- config/zsh/aliases/laravel.zsh
+++ config/zsh/aliases/laravel.zsh
@@ -1,19 +1,20 @@
 # Laravel
-alias pa="herd php artisan"
-alias migrate="herd php artisan migrate"
-alias seed="herd php artisan seed"
-alias srv="herd php artisan serve"
-alias mfs="herd php artisan migrate:fresh --seed"
-alias sail="./vendor/bin/sail"
-alias tinker="herd php artisan tinker"
+alias pa="php artisan"
+alias seed="php artisan seed"
+alias srv="php artisan serve"
+alias mfs="php artisan migrate:fresh --seed"
+alias tinker="php artisan tinker"
 alias a="artisan"
+alias sail="./vendor/bin/sail"
+alias sa="sail artisan"
+alias sam="sail artisan migrate"
 
 # Larastan
 alias larastan="vendor/bin/phpstan analyse"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
-alias composer="herd composer"
+alias composer="composer"
 alias php="herd php"
 
 # JS
