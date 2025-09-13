a598bee83d9ab5d32398e5852cb84e9f0863171c
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-09-12 20:51:49 -0400

    refactor: Rename misc-stuff.zsh to fallbacks.zsh and clean up

--- config/zsh/helpers/fallbacks.zsh
+++ config/zsh/helpers/fallbacks.zsh
@@ -1,7 +1,5 @@
 
-if hash thefuck 2> /dev/null; then;
-  eval $(thefuck --alias)
-fi
+# Command fallbacks and aliases
 
 # Fix default editor, for systems without nvim installed
 if ! hash nvim 2> /dev/null; then
